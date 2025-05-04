bda56e089926e0db77b72da1fb66b11ce75d4e08
Kawann <kawannmarcelo7@gmail.com>
2025-05-03 21:09:54 -0300

    feat: add JWT generation utility

---
+++ internal/utils/jwt.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID string) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "default_secret"
+	}
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp": time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
